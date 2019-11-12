1a36b82aabe41198d3ec0f5413c9c186f6f9d00d
Zameer Ansari <zameer.ansari@instarem.com>
2019-11-12 21:25:26 +0530

    type support +

--- dist/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var TradingViewEmbed_1 = require("./TradingViewEmbed");
-exports.TradingViewEmbed = TradingViewEmbed_1.TradingViewEmbed;
-exports.widgetType = TradingViewEmbed_1.widgetType;
-//# sourceMappingURL=index.js.map
