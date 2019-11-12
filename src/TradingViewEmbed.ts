03e57c52b9cdb45a44d8f4ef1f2f77fcbd6fefa5
Zameer Ansari <zameer.ansari@instarem.com>
2019-11-12 22:32:29 +0530

    parameterized

--- src/TradingViewEmbed.ts
+++ src/TradingViewEmbed.ts
@@ -1,14 +1,21 @@
-const generateSymbolInfoChart = (elementId: string, symbol: string) => {
+const generateSymbolInfoChart = (
+    elementId: string,
+    symbol: string,
+    width: string,
+    locale: string,
+    colorTheme: string,
+    isTransparent: boolean
+) => {
     if (document.getElementById(elementId) && document.getElementById(elementId)!.innerHTML === "") {
         const script = document.createElement('script');
         script.src = 'https://s3.tradingview.com/external-embedding/embed-widget-symbol-info.js'
         script.async = true;
         script.innerHTML = JSON.stringify({
             symbol: symbol,
-            width: "100%",
-            locale: "in",
-            colorTheme: "light",
-            isTransparent: "true"
+            width: width,
+            locale: locale,
+            colorTheme: colorTheme,
+            isTransparent: isTransparent
         });
         document.getElementById(elementId)!.appendChild(script);
     }
